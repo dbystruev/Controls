5437b0d8db9f66f166d9d5ee1889f26059100116
Denis Bystruev <dbystruev@me.com>
2019-01-19 16:22:40 +0300

    As of end of lesson 19.01.2019

--- Controls/ViewController.swift
+++ Controls/ViewController.swift
@@ -13,15 +13,43 @@ class ViewController: UIViewController {
     var count = 0
     
     @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var `switch`: UISwitch!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateUI()
+    }
+    
+    func updateUI() {
+        button.setTitle("Нажато: \(count)", for: .normal)
+        label.text = `switch`.isOn ? "Вкл" : "Выкл"
+        
+        view.backgroundColor = UIColor(
+            hue: 0.5,
+            saturation: 0.5,
+            brightness: CGFloat(slider.value),
+            alpha: 1
+        )
     }
 
     @IBAction func buttonPressed() {
         count += 1
-        button.setTitle("Нажато: \(count)", for: .normal)
+        
+        if count % 2 == 0 {
+            `switch`.isOn = !`switch`.isOn
+        }
+        
+        updateUI()
+    }
+    
+    @IBAction func switchToggled() {
+        updateUI()
+    }
+    
+    @IBAction func sliderMoved() {
+        updateUI()
     }
 }
 
